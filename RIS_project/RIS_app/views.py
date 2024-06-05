fd09955954dbcd160a66afc250ede5cce8cb956b
Nikk434 <nskshirsagar434@gmail.com>
2024-06-05 18:22:35 +0530

    Logic for login and registration

--- RIS_project/RIS_app/views.py
+++ RIS_project/RIS_app/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login , logout
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
+    
+    if request.method == 'POST':
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # password check        
+        if len(password)<8:
+            messages.error(request,"Password must be atleast 8 charecters")
+            return redirect('register')
+        # user check
+        get_users = User.objects.filter(username=name)
+        if get_users:
+            messages.error(request,"User already exist !")
+            return redirect('register')
+
+
+        new_user = User.objects.create_user(username=name, email=email, password=password)
+        new_user.save()
+        messages.success(request, "Registration suceessful! Log in now ")
+        # return render(request, 'todoapp/login.html', {})
+        return redirect('login')
+
+    return render(request, 'register.html', {})
+
+def login(request):
+    # if request.user.is_authenticated:
+    #     return redirect('home-page')
+    
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        check_user = authenticate(username=username, password = password)
+        if check_user is not None:
+            auth_login(request, check_user)
+            return redirect('home-page')
+        else:
+            messages.error(request, "User does not exist")
+            return redirect('login')
+    
+    return render(request, 'login.html',{})
+
