b25c5f2b4185ff100b1fb659e277f810d6b7c648
Nikk434 <nskshirsagar434@gmail.com>
2024-06-05 18:21:57 +0530

    Logic for login and registration

--- RIS_project/RIS_app/urls.py
+++ RIS_project/RIS_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('', views.home, name='home-page'),    
+    path('', views.home, name='home-page'),  
+    path('login/', views.login, name='login'),
+    # path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+      
 ]
