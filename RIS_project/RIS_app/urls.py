f4bfdcf371a1827526c96f4e9053e992e9fa2c6e
Nikk434 <nskshirsagar434@gmail.com>
2024-06-06 18:05:41 +0530

    updated urls

--- RIS_project/RIS_app/urls.py
+++ RIS_project/RIS_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     # path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
+    path('add_new_patient/', views.add_new_patient, name='add_new_patient'),
       
 ]
