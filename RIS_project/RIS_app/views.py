db7790b1582045383ac364b048b28b6101d278dc
Nikk434 <nskshirsagar434@gmail.com>
2024-06-11 19:34:38 +0530

    added view to generate a report

--- RIS_project/RIS_app/views.py
+++ RIS_project/RIS_app/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 import pymongo
 from dotenv import load_dotenv
 import os 
+from docx import Document
+
 
 load_dotenv('D:\DJANGO\.env')
 
@@ -75,7 +77,68 @@ def add_new_patient(request):
     return render(request, 'add_new_patient.html')
     
 def add_report_xray(request):
-    pass
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('Patient DOB')
+        gender = request.POST.get('gender')
+        date = request.POST.get('exam_date')
+        referringPhysicianName = request.POST.get('referringPhysicianName')
+        contactInformation = request.POST.get('contactInformation')
+        typeOfExamination = request.POST.get('typeOfExamination')
+        bodyPartExamined = request.POST.get('bodyPartExamined')
+        clinicalIndication = request.POST.get('clinicalIndication')
+        xrayMachineModel = request.POST.get('xrayMachineModel')
+        exposureParameters = request.POST.get('exposureParameters')
+        radiographerName = request.POST.get('radiographerName')
+        findings = request.POST.get('findings')
+        impression = request.POST.get('impression')
+        recommendations = request.POST.get('recommendations')
+        radiologistName = request.POST.get('radiologistName')
+        dateOfReport = request.POST.get('dateOfReport')
+
+        def get_user_inpu():
+            patient_info = {
+                'Patient Name': name,
+                'Patient age': age,
+                'Patient Gender': gender,
+                'Examination Date': date,
+                'Referring Physician Name': referringPhysicianName,
+                'Contact Information': contactInformation,
+                'Type of X-Ray': typeOfExamination,
+                'Body Part': bodyPartExamined,
+                'Clinical Indication/Reason for Exam': clinicalIndication,
+                'Model': xrayMachineModel,
+                'Exposure Parameters': exposureParameters,
+                'Name':radiographerName,
+                'findings': findings,
+                'Impression': impression,
+                'Recommendations': recommendations,
+                'Radiologist Name': radiologistName,
+                'Date of Report': dateOfReport
+            }
+            return patient_info    
+        
+        def replace_placeholders(template_path, output_path, replacements):
+            doc = Document(template_path)
+            for paragraph in doc.paragraphs:
+                for key, value in replacements.items():
+                    if key in paragraph.text:
+                        paragraph.text = paragraph.text.replace(f'[{key}]', value)
+            doc.save(output_path)
+        
+        template_path = 'D:\DJANGO\\report_template.docx'  # Path to your template
+        output_path = 'report.docx'  # Path where the filled document will be saved
+
+    # Get user     
+        user_info = get_user_inpu()
+
+    # Replace placeholders and save the document
+        replace_placeholders(template_path, output_path, user_info)
+
+        print(f"Document saved as {output_path}")
+
+    return redirect('home-page')
+
 
 def register(request):
     if request.user.is_authenticated:
