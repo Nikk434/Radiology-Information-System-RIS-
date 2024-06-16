5579768dbad9f3bfb1a003d417a417a6e657ccdd
Nikk434 <nskshirsagar434@gmail.com>
2024-06-16 11:31:42 +0530

    url for upload view

--- RIS_project/RIS_app/urls.py
+++ RIS_project/RIS_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('add_new_patient/', views.add_new_patient, name='add_new_patient'),
     path('add_report_xray/', views.add_report_xray, name='add_report_xray'),
-
+    path('upload/', views.upload, name='upload')
 ]
