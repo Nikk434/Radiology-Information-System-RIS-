9f3f10d3473c1edbd54a8ff3d93dc9a4201dc71a
Nikk434 <nskshirsagar434@gmail.com>
2024-06-16 11:32:22 +0530

    .

--- RIS_project/RIS_app/models.py
+++ RIS_project/RIS_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Document(models.Model):
-    # title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     uploaded_file = models.FileField(upload_to='x_ray/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
