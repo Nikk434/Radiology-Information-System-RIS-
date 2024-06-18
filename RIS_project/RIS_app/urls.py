7be297be6e01663ee7e47482e8d86b027de257a6
Nikk434 <nskshirsagar434@gmail.com>
2024-06-18 12:33:19 +0530

    url for search view

--- RIS_project/RIS_app/urls.py
+++ RIS_project/RIS_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('add_new_patient/', views.add_new_patient, name='add_new_patient'),
     path('add_report_xray/', views.add_report_xray, name='add_report_xray'),
-    path('upload/', views.upload, name='upload')
+    path('upload/', views.upload, name='upload'),
+    path('search_for/', views.search_for, name='search_for'),
+
 ]
