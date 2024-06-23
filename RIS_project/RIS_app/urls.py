e17d30110fbf11a51206a48e6bff16e2aa2582f3
Nikk434 <nskshirsagar434@gmail.com>
2024-06-23 20:57:32 +0530

    log out

--- RIS_project/RIS_app/urls.py
+++ RIS_project/RIS_app/urls.py
@@ -4,7 +4,7 @@ from. import views
 urlpatterns = [
     path('', views.home, name='home-page'),  
     path('login/', views.login, name='login'),
-    # path('logout/', views.logout_view, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('add_new_patient/', views.add_new_patient, name='add_new_patient'),
     path('add_report_xray/', views.add_report_xray, name='add_report_xray'),
